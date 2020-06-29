603c2ae472005c2df647c8fc2e4cebffc732706c
Hasantha malshan <hasanthamalshan07@gmail.com>
2020-06-29 14:04:07 +0530

    connected to the database

--- api/handlers/auth.js
+++ api/handlers/auth.js
@@ -4,3 +4,7 @@ const express = require("express");
 exports.CompanyRegistration = (req, res) => {
     res.send('company resister api set up');
 }
+
+exports.CompanyLogin = (req, res) => {
+    res.send('company login api set up');
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -6,6 +6,7 @@ const authRoutes = require("./auth")
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../../index.html'));
 });
+
 router.use('/auth', authRoutes);
 
 module.exports = router;
