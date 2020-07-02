402ecc3a7ca50df8afca6d0c939f627c50ae2f61
Hasantha malshan <hasanthamalshan07@gmail.com>
2020-07-02 15:48:40 +0530

    validate data and password hashing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use("/", routes)
 
 app.listen(Config.env_data.PORT, () => {
-    console.log(`Server started on ${Config.env_data.PORT}`);
+    console.log(`Server started on port ${Config.env_data.PORT}`);
     pool.connect()
         .then(() => console.log('connected to the database...'))
         .catch(() => console.log('error connecting to the database ...'));
