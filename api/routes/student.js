653aa9a4d8a759b26413f49f3a433b0efbb4af44
Amanda <thirunihp@gmail.com>
2020-10-10 12:50:54 +0530

    Enrolled students

--- api/routes/student.js
+++ api/routes/student.js
@@ -6,5 +6,8 @@ router.post("/add", handler.AddNewStudent);
 router.post("/login" , handler.login);
 router.post("/setPassword" , handler.setPassword);
 router.post("/forgotpassword" , handler.forgotPassword)
+router.get("/getAll", handler.getAllData)
+router.put("/update", handler.updateStudent)
+router.post("/addNewStudent", handler.addNewStudent)
 
 module.exports = router;
