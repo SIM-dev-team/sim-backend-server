e406321c59a76a3e311cbeaf574519ddb0145040
Tharindu Basanayaka <tbasnayaka7@gmail.com>
2020-10-10 13:13:17 +0530

    get Approved comapny function  and route added

--- api/routes/company.js
+++ api/routes/company.js
@@ -3,10 +3,11 @@ const router = require("express").Router();
 const handler = require("../handlers/company");
 
 router.get("/get/:token", handler.GetCompanyData);
-router.get("/getAll" , handler.GetAll);
-router.post("/update" , handler.UpdateProfile);
-router.get("/getCompany/:id" , handler.GetCompanyById)
+router.get("/getAll", handler.GetAll);
+router.post("/update", handler.UpdateProfile);
+router.get("/getCompany/:id", handler.GetCompanyById)
 router.post("/approveCompany", handler.ApproveCompany);
 router.post("/sendMailtoCompany", handler.SendMail);
+router.get("/getApprovedCompany", handler.GetApprovedCompanyList);
 
 module.exports = router;
