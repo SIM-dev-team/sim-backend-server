d6eb61809188c18714e5e2b6fc44f41f5fb3a5c8
Hasantha malshan <hasanthamalshan07@gmail.com>
2020-10-10 19:24:29 +0530

    apply for advert function

--- api/handlers/adverts.js
+++ api/handlers/adverts.js
@@ -112,6 +112,26 @@ exports.GetAllAdverts = ( req , res) => {
     }
 }
 
+exports.GetAllApproved = (req , res) =>{
+    try {
+        pool.connect((err, client, done) => {
+            if (err) res.send('error connecting to database...');
+            else{
+            client.query(`SELECT * FROM adverts WHERE status = 'approved'`, (errp, resp) => {
+                client.release();
+                if (errp) {
+                    res.send('no data');
+                } else {
+                    res.status(200).json(resp.rows);
+                }
+            });
+        }
+        });
+    } catch (e) {
+        return res.status(400).send('invalid token');
+    }
+}
+
 // get all adverts by a given company
 exports.GetAllAdvertsByCompany = ( req , res) => {
     const token = req.body.token;
@@ -227,7 +247,54 @@ exports.DeclineAdvert = ( req , res ) => {
 
 //apply for a advert by a student
 exports.ApplyForAdvert = ( req , res ) => {
-    console.log('apply for advert works');
+    try {
+        console.log(req.body.Data);
+        const verified = jwt.verify(req.body.Data.token, env_data.JWT_TOKEN);
+        pool.connect((err, client, done) => {
+            if (err) res.send('error connecting to database...');
+            else{
+                client.query(`INSERT INTO advert_student(id,s_id , a_id , cv_link)VALUES(nextval('student_advert_sequence'),$1,$2,$3) RETURNING *`,[verified.id , req.body.Data.advert , req.body.Data.link ], (errp, resp) => {
+                    
+                    if (errp) {
+                        res.send(errp);
+                    } else {
+                        client.query(`UPDATE adverts SET no_of_applicants = no_of_applicants + 1 WHERE ad_id = ${req.body.Data.advert} RETURNING *`, (errpp, resp) => {
+                            if(errpp){
+                                res.send(errpp); 
+                            }else{
+                                res.status(200).json(resp.rows[0]);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    } catch (e) {
+        return res.status(400).send('invalid token');
+    }
+}
+
+//get applied adverts by a student
+exports.GetAppliedAdverts = ( req , res ) => {
+    try {
+        const verified = jwt.verify(req.body.token, env_data.JWT_TOKEN);
+        pool.connect((err, client, done) => {
+            if (err) res.send('error connecting to database...');
+            else{
+                console.log("token >" , verified.id);
+                client.query(`SELECT * FROM  advert_student WHERE s_id = '${verified.id}'`, (errp, resp) => {
+                    client.release();
+                    if (errp) {
+                        res.send(errp);
+                    } else {
+                        res.status(200).json(resp.rows);
+                    }
+                });
+            }
+        });
+    } catch (e) {
+        return res.status(400).send('invalid token');
+    }
 }
 
 //publish adverts for students by PDC
@@ -237,7 +304,7 @@ exports.PublishAdverts = (req , res) => {
         pool.connect((err, client, done) => {
             if (err) res.send('error connecting to database...');
             else{
-            client.query(`UPDATE advert SET status = published' WHERE status = approved RETURNING *`, (errp, resp) => {
+            client.query(`UPDATE advert SET status = published WHERE status = approved RETURNING *`, (errp, resp) => {
                 client.release();
                 if (errp) {
                     res.send('no data');
--- api/routes/advert.js
+++ api/routes/advert.js
@@ -5,15 +5,17 @@ const handler = require("../handlers/adverts");
 router.post("/create", handler.CreateAdvert);
 router.get("/get/:id", handler.GetAdvert);
 router.get("/getAll" , handler.GetAllAdverts);
+router.get("/getAllApproved" , handler.GetAllApproved);
 router.post("/getAdvertsByCompany/" , handler.GetAllAdvertsByCompany);
 router.get("/getAdvertsByCompanyId/:id" , handler.GetAllAdvertsByCompanyId);
 router.get("/getAdvertsByCategory/:cat_id" , handler.GetAllAdvertsByCategory);
 router.put("/ApproveAdvert" , handler.ApproveAdvert);
 router.put("/DeclineAdvert" , handler.DeclineAdvert);
-router.post("/Apply/:id" , handler.ApplyForAdvert);
+router.post("/Apply" , handler.ApplyForAdvert);
 router.put("/publish" , handler.PublishAdverts);
 router.post("/addCategory" , handler.AddNewCategory);
 router.get("/getCategory/:id" , handler.GetCategory);
 router.get("/getCategories" , handler.GetCategories);
+router.post("/getAppliedAdverts" , handler.GetAppliedAdverts);
 
 module.exports = router;
